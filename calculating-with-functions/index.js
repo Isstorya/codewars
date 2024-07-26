a1ff5794a4fe6cc11b6bdaaa22725c9e8f672687
isstorya <stolyarovI.teacher@filial.gpi>
2024-07-26 12:28:20 +0300

    init

---
+++ calculating-with-functions/index.js
@@ -0,0 +1,43 @@
+function zero(p = "") {
+  return eval(`0 ${p}`);
+}
+function one(p = "") {
+  return eval(`1 ${p}`);
+}
+function two(p = "") {
+  return eval(`2 ${p}`);
+}
+function three(p = "") {
+  return eval(`3 ${p}`);
+}
+function four(p = "") {
+  return eval(`4 ${p}`);
+}
+function five(p = "") {
+  return eval(`5 ${p}`);
+}
+function six(p = "") {
+  return eval(`6 ${p}`);
+}
+function seven(p = "") {
+  return eval(`7 ${p}`);
+}
+function eight(p = "") {
+  return eval(`8 ${p}`);
+}
+function nine(p = "") {
+  return eval(`9 ${p}`);
+}
+
+function plus(p) {
+  return `+ ${p}`;
+}
+function minus(p) {
+  return `- ${p}`;
+}
+function times(p) {
+  return `* ${p}`;
+}
+function dividedBy(p) {
+  return `/ ${p}`;
+}
