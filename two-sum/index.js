811d0122eacb6d1c7bc9269ffb0c5452e6781625
isstorya <stolyarovI.teacher@filial.gpi>
2024-07-26 11:51:26 +0300

    realize common function

--- two-sum/index.js
+++ two-sum/index.js
@@ -1,5 +1,14 @@
 function twoSum(numbers, target) {
-  return arguments;
+  let result = [];
+
+  numbers.forEach((number, index) => {
+    numbers.forEach((targetNumber, targetIndex) => {
+      if (numbers[index] === numbers[targetIndex]) return;
+      if (number + targetNumber === target) result = [index, targetIndex];
+    });
+  });
+
+  return result;
 }
 
-console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([2, 2, 3], 4));
